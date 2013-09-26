8a0a53c2c40c1a7cfacea8e3f0f7d1cfa0cf10f8
Jim Rosser <jarosser06@gmail.com>
2013-09-26 12:08:40 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'nfs_media'
+maintainer       'Texas A&M'
+maintainer_email 'jarosser06@arch.tamu.edu'
+license          'MIT'
+description      'Installs/Configures nfs_media'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
